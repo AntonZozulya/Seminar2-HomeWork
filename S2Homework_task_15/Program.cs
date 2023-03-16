89c638bfd40a653f76d55e80be16184bbe72f154
AntonZozulya <antonzo93@mail.ru>
2023-03-16 17:29:13 +0300

    add task_15

---
+++ S2Homework_task_15/Program.cs
@@ -0,0 +1,39 @@
+﻿// ПЕРВЫЙ ВАРИАНТ ПРОГРАММЫ БЕЗ С СООБЩЕНИЕ ЧТО 1-5 БУДНИЕ ДНИ
+
+// Console.Write("Введите день недели: ");
+//     int n = Convert.ToInt32(Console.ReadLine()!); 
+//     while (n < 1 || n > 7) // || - это означает "ИЛИ" это алгебра
+//     {
+//         Console.Write("Вы ошиблись, \nВведите день недели: "); // \n - это переход на новую строчку
+//         n = Convert.ToInt32(Console.ReadLine()!);
+//     }
+//     if(n == 1)
+//         Console.Write("НЕ ВЫХОДНОЙ ДЕНЬ");
+//     else if(n == 2) // else if - иначе если
+//         Console.Write("НЕ ВЫХОДНОЙ ДЕНЬ");
+//     else if(n == 3) // else if - иначе если
+//         Console.Write("НЕ ВЫХОДНОЙ ДЕНЬ");
+//     else if(n == 4) // else if - иначе если
+//         Console.Write("НЕ ВЫХОДНОЙ ДЕНЬ");
+//     else if(n == 5) // else if - иначе если
+//         Console.Write("НЕ ВЫХОДНОЙ ДЕНЬ");
+//     else if(n == 6) // else if - иначе если
+//         Console.Write("ВЫХОДНОЙ ДЕНЬ");
+//     else
+//         Console.Write("ВЫХОДНОЙ ДЕНЬ");
+
+
+
+// ВТОРОЙ ВАРИАНТ ПРОГРАММЫ БЕЗ СООБЩЕНИЯ О БУДНЯХ ДНЯХ
+
+Console.Write("Введите день недели: ");
+    int n = Convert.ToInt32(Console.ReadLine()!); 
+    while (n < 6 || n > 7) // || - это означает "ИЛИ" это алгебра
+    {
+        Console.Write("Вы ошиблись, \nВведите день недели: "); // \n - это переход на новую строчку
+        n = Convert.ToInt32(Console.ReadLine()!);
+    }
+    if(n == 6)
+        Console.Write("ВЫХОДНОЙ ДЕНЬ");
+    else 
+        Console.Write("ВЫХОДНОЙ ДЕНЬ");
